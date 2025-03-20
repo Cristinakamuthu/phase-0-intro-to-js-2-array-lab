68f4a9f949974d2345f0b05b8efb061aa90ebb0b
Cristina <kamuthucristina@gmail.com>
2025-03-20 06:51:09 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function beforeEach () {
+    //cats.length = 0
+// console.log(cats.push)
+}
+
+function destructivelyAppendCat () {
+    cats.push ( "Ralph")
+    console.log(cats.push)
+}
+
+function destructivelyPrependCat (){
+    cats.unshift ("Bob")
+        console.log (cats.unshift)
+    
+}function destructivelyRemoveLastCat (){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat () {
+    cats.shift()
+}
+
+
+function appendCat () {
+    return [...cats ,"Broom"]
+  }
+  function prependCat (){
+return ["Arnold", ...cats]
+ }
+ function removeLastCat () {
+   return  cats.slice(0,-1)
+ }
+ function removeFirstCat () {
+    return  cats.slice(1)
+  }
+
+
